bb9855accba99ff398d1b831484d5214aa11247b
SawshaDev <85613286+SawshaDev@users.noreply.github.com>
2022-10-10 21:24:09 -0700

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import osu
-
-async def main():
-    client = osu.Client(client_secret="V9lAyJIUVJiyUTFr5iDHfrwV5qOmbXV6shnZe3Ot", client_id=15748)
-    print((await client.fetch_user("Sawsha")).username)
-    await client.close()
-
-asyncio.run(main())
